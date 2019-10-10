5eac684efbe3e5576bf958321ad377d04d372b59
ErgoFriend <erg.friend@gmail.com>
2019-10-10 22:21:22 +0900

    tmp intl

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { IntlProvider, FormattedMessage } from 'react-intl';
+
 const App: React.FC = () => {
+  const state = {
+    name: 'Eric',
+    unreadCount: 1000,
+  }
+  const { name, unreadCount } = state;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>
+          <FormattedMessage
+            id="welcome"
+            defaultMessage={`Hello {name}, you have {unreadCount, number} {unreadCount, plural,
+                      one {message}
+                      other {messages}
+                    }`}
+            values={{ name: <b>{name}</b>, unreadCount }}
+          />
         </p>
         <a
           className="App-link"
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import { IntlProvider, FormattedMessage } from 'react-intl'
+
+ReactDOM.render(
+    <IntlProvider locale={'ja'}>
+        <App />
+    </IntlProvider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
