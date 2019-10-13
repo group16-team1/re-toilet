709c2b6167b8975750978364c7a1dd6260708a34
ErgoFriend <erg.friend@gmail.com>
2019-10-13 15:31:00 +0900

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer: React.FC = () => {
+  return (
+    <div>
+      <div>Icons made by <a href="https://www.flaticon.com/authors/srip" title="srip">srip</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+    </div>
+  )
+}
+
+export default Footer 
