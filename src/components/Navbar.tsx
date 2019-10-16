690a9920450460fe6d7c08c635c86ec3fe8329db
ErgoFriend <erg.friend@gmail.com>
2019-10-16 22:33:17 +0900

    fix navbar link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar: React.FC = () => {
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
+        <a className="navbar-item" href={window.location.origin}>
           <p className="title">re:toire</p>
         </a>
       </div>
