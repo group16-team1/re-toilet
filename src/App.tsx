b48e3f522804ab4e0ce209c2443eb5ccc17ab980
ErgoFriend <erg.friend@gmail.com>
2019-10-17 13:11:32 +0900

    replace BrowserRouter with HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import LocaleBtn from './components/LocaleBtn'
@@ -10,7 +10,7 @@ import Home from './pages/Home'
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename='/'>
       <div className="App">
         <Navbar />
         <Switch>
@@ -20,7 +20,7 @@ const App: React.FC = () => {
         <Footer />
         <LocaleBtn />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
