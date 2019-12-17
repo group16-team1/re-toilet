1d7fbbedd803384656868ae8369f29cd943fa51e
ErgoFriend <erg.friend@gmail.com>
2019-12-17 20:05:18 +0900

    export components

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+import About from "./About";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import HeroList from "./HeroList";
+import Icon from "./Icon";
+import IconList from "./IconList";
+import LocaleBtn from "./LocaleBtn";
+import Navbar from "./Navbar";
+
+export {About, Footer, Hero, HeroList, Icon, IconList, LocaleBtn, Navbar};
