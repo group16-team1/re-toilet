a037407df21aae874c37534ab7f7982219144e72
ErgoFriend <erg.friend@gmail.com>
2019-12-17 21:38:54 +0900

    add illustlation license

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,6 +9,7 @@ const Footer: React.FC = () => {
           <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
           is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
         </p>
+        <a href="https://www.vecteezy.com/free-vector/toilets">Toilets Vectors by Vecteezy</a>
         <p>Icons made by <a href="https://www.flaticon.com/authors/srip" title="srip">srip</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
       </div>
     </footer>
