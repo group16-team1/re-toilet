7f09635c33c850421de4a83c2d0e63bb516ca8fb
ErgoFriend <erg.friend@gmail.com>
2019-12-17 21:47:29 +0900

    fix About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,20 +3,22 @@ import { Link } from 'react-router-dom'
 
 export default function About() {
   return (
-      <div className="hero-body">
-        <div className="container has-text-centered">
-          <div className="column is-6 is-offset-3">
-            <h1 className="title">
-              re:Toilet
+    <div className="hero-body">
+      <div className="container has-text-centered">
+        <div className="column is-6 is-offset-3">
+          <h1 className="title">
+            re:Toilet
                         </h1>
-            <h2 className="subtitle" style={{marginTop: '1rem'}}>
-              私たちは視覚デザインから、トイレの利便性とマナーの向上を目指します。
-              <Link to={'/about'} >
-                  詳しく読む
+          <h2 className="subtitle" style={{ marginTop: '1rem' }}>
+            私たちは視覚デザインから、トイレの利便性とマナーの向上を目指します。
+            <br />
+            <br />
+            <Link to={'/about'}>
+              詳しく読む
               </Link>
-            </h2>
-          </div>
+          </h2>
         </div>
       </div>
+    </div>
   )
 }
