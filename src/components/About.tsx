6311457edbf09f0434cc446c91c3a787c61bd1ba
ErgoFriend <erg.friend@gmail.com>
2019-12-17 21:39:18 +0900

    change style

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,9 +3,20 @@ import { Link } from 'react-router-dom'
 
 export default function About() {
   return (
-    <div>
-      <p>私たちは、視覚デザインからトイレの利便性とマナーの向上を目指します。</p>
-      <Link to={'/about'} >詳しく読む</Link>
-    </div>
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <div className="column is-6 is-offset-3">
+            <h1 className="title">
+              re:Toilet
+                        </h1>
+            <h2 className="subtitle" style={{marginTop: '1rem'}}>
+              私たちは視覚デザインから、トイレの利便性とマナーの向上を目指します。
+              <Link to={'/about'} >
+                  詳しく読む
+              </Link>
+            </h2>
+          </div>
+        </div>
+      </div>
   )
 }
