4dd515ceb26b82e99c4fd55a26789e595355988f
ErgoFriend <erg.friend@gmail.com>
2019-12-18 13:15:35 +0900

    replace icons

--- src/components/IconList.tsx
+++ src/components/IconList.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 import Icon from './Icon'
-import { assets } from '../assets/index'
+import { assets4 } from '../assets/index'
 
 const IconList: React.FC = () => {
   return (
     <div className="columns is-tablet is-multiline">
-      {assets.map(image => {
+      {assets4.map(image => {
         return <Icon logo={image} key={image} />
       })}
     </div>
