8366b306f8bc2fb4df0b8643aa89a5c6560d8e93
ErgoFriend <erg.friend@gmail.com>
2019-12-18 13:15:26 +0900

    replace images

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,8 @@ import { Footer, LocaleBtn } from './components'
 import Home from './pages/Home'
 import About from './pages/About'
 import Hero from './pages/Hero'
-import { manner, clean, eco } from './assets/index'
+
+import { hand, cleaning, qe } from './assets/index'
 
 const App: React.FC = () => {
   // load epic and reducer
@@ -21,9 +22,9 @@ const App: React.FC = () => {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
-          <Route exact path="/manner" render={() => <Hero key="manner" title={'manner'} image={manner} />} />
-          <Route exact path="/clean" render={() => <Hero key="clean" title={'clean'} image={clean} />} />
-          <Route exact path="/eco" render={() => <Hero key="eco" title={'eco'} image={eco} />} />
+          <Route exact path="/manner" render={() => <Hero key="manner" title={'manner'} image={hand} />} />
+          <Route exact path="/clean" render={() => <Hero key="clean" title={'clean'} image={cleaning} />} />
+          <Route exact path="/eco" render={() => <Hero key="eco" title={'eco'} image={qe} />} />
         </Switch>
         <Footer />
         <LocaleBtn />
