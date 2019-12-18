6cf3a03a37406511ea863d5e1bf9f52ff2760bc5
ErgoFriend <erg.friend@gmail.com>
2019-12-18 13:15:00 +0900

    add new assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -7,10 +7,13 @@ export const open_seat = require("./open-seat.png");
 export const stop = require("./stop.png");
 export const wash = require("./wash.png");
 
+export const flush_big2 = require("./flush_big2.png");
+export const flush_small2 = require("./flush_small2.png");
+
 export const assets = [
   dry,
-  flush_toilet_big,
-  flush_toilet_small,
+  flush_big2,
+  flush_small2,
   front_wash,
   open_lid,
   open_seat,
@@ -25,3 +28,20 @@ export const eco = require("./images/re_eco.jpeg");
 export const cleaning = require("./Cleaning_Icons/Cleaning-Icons.jpg");
 export const hand = require("./Hand_Hygiene_Vector/Hand-Hygiene-Vector.png");
 export const qe = require("./QE/QE.jpg");
+
+export const back_button = require("./left-arrow.svg");
+
+export const close4 = require("./images.v4/close4.png");
+export const f_wash4 = require("./images.v4/f_wash4.png");
+export const flush_big4 = require("./images.v4/flush_big4.png");
+export const flush_small4 = require("./images.v4/flush_small4.png");
+export const open4 = require("./images.v4/open4.png");
+export const wash4 = require("./images.v4/wash4.png");
+export const assets4 = [
+  close4,
+  f_wash4,
+  flush_big4,
+  flush_small4,
+  open4,
+  wash4
+];
