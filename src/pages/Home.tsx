a44be9db12934d717ec6d1b43eb53d0677c3c7a6
ErgoFriend <erg.friend@gmail.com>
2019-12-18 13:16:45 +0900

    remove i18n

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,17 @@
 import React from 'react'
 
-import { getLanguageState } from '../store/language'
-
-import { FormattedMessage } from 'react-intl';
-
 import { About, HeroList, IconList } from '../components'
 
-import { hello } from '../locales/hello'
-
 const Home: React.FC = () => {
-  // get state from store
-  const { language } = getLanguageState.useState();
-
   return (
     <header className="App-header">
-      <About />
-      <HeroList />
-      <div className="subtitle">
-        <FormattedMessage
-          id='hello_name'
-          description='say hello to wang.'
-          defaultMessage={hello[language]}
-          values={{ my_name: 'INIAD' }}
-        />
+      <div className="container">
+        <div className="section">
+          <About />
+          <HeroList />
+          <IconList />
+        </div>
       </div>
-      <IconList />
     </header>
   )
 }
