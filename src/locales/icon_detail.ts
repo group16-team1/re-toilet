441b6272c8240b2e74afe14b9c57bc6720d7d6ac
ErgoFriend <erg.friend@gmail.com>
2020-01-08 00:17:06 +0900

    icon i18n

--- src/locales/icon_detail.ts
+++ src/locales/icon_detail.ts
@@ -3,86 +3,86 @@ import { IconListI } from "./type";
 export const local_icon_detail: IconListI = {
   flush_small: {
     title: {
-      en: "English",
-      ja: "水量少なめ",
-      ct: "繁体中文",
-      cs: "簡体中文"
+      en: "Shed small",
+      ja: "流す　小",
+      ct: "小棚",
+      cs: "小棚"
     },
     content: {
-      en: "English",
-      ja: "日本語",
-      ct: "繁体中文",
-      cs: "簡体中文"
+      en: "Less water",
+      ja: "水量少なめ",
+      ct: "少喝水",
+      cs: "少喝水"
     }
   },
   flush_big: {
     title: {
-      en: "English",
-      ja: "水量　大",
-      ct: "繁体中文",
-      cs: "簡体中文"
+      en: "Shed large",
+      ja: "流す　大",
+      ct: "大棚",
+      cs: "大棚"
     },
     content: {
-      en: "English",
-      ja: "日本語",
-      ct: "繁体中文",
-      cs: "簡体中文"
+      en: "Water volume large",
+      ja: "水量　多め",
+      ct: "水量大",
+      cs: "水量大"
     }
   },
   wash: {
     title: {
-      en: "English",
+      en: "Washlet",
       ja: "ウォシュレット",
-      ct: "繁体中文",
-      cs: "簡体中文"
+      ct: "卫洗丽",
+      cs: "衛洗麗"
     },
     content: {
-      en: "English",
-      ja: "日本語",
-      ct: "繁体中文",
-      cs: "簡体中文"
+      en: "Wash with water",
+      ja: "水で洗う",
+      ct: "用水冲洗",
+      cs: "用水沖洗"
     }
   },
   wash_front: {
     title: {
-      en: "English",
+      en: "Washlet for women",
       ja: "女性用ウォシュレット",
-      ct: "繁体中文",
-      cs: "簡体中文"
+      ct: "女士卫洗丽",
+      cs: "女士衛洗麗"
     },
     content: {
-      en: "English",
-      ja: "日本語",
-      ct: "繁体中文",
-      cs: "簡体中文"
+      en: "Wash with water",
+      ja: "水で洗う",
+      ct: "用水冲洗",
+      cs: "用水沖洗"
     }
   },
   open: {
     title: {
-      en: "English",
-      ja: "日本語",
-      ct: "繁体中文",
-      cs: "簡体中文"
+      en: "Open the sheet",
+      ja: "シートを開ける",
+      ct: "打开表",
+      cs: "打開表"
     },
     content: {
-      en: "English",
-      ja: "日本語",
-      ct: "繁体中文",
-      cs: "簡体中文"
+      en: "Raise the sheet to keep it clean",
+      ja: "汚れないようにシートを上げよう",
+      ct: "抬起床单以保持干净",
+      cs: "抬起床單以保持乾淨"
     }
   },
   open_lid: {
     title: {
-      en: "English",
-      ja: "日本語",
-      ct: "繁体中文",
-      cs: "簡体中文"
+      en: "Open the lid",
+      ja: "蓋を開ける",
+      ct: "打开盖子",
+      cs: "打開蓋子"
     },
     content: {
-      en: "English",
-      ja: "日本語",
-      ct: "繁体中文",
-      cs: "簡体中文"
+      en: "Close it when you open it",
+      ja: "開けたら閉めよう",
+      ct: "打开时将其关闭",
+      cs: "打開時將其關閉"
     }
   }
 } as IconListI;
